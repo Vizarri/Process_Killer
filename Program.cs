0a5a586ebc5bfab403c36ed62377aa2a2ee0edda
Vizarri <82329177+Vizarri@users.noreply.github.com>
2021-07-18 20:31:10 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Process_Killer
             Watcher watcher = new Watcher(args[0], int.Parse(args[1]));
             bool flag = true;
 
-            //реализация постоянной проверки через каждые X минут , который были заданы
+            //реализация постоянной проверки через каждые X минут , которые были заданы 
             while (flag)
             {
                 System.Threading.Thread.Sleep(int.Parse(args[2]) * 60000);
